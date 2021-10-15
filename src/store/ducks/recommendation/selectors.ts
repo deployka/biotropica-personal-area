f2f1620d793cbd787f9b4cd298d6e9add6a573a1
MirageNShift <desertirpipi@gmail.com>
2021-10-15 14:36:51 +0300

    Добавил сторы для анализов и рекомендаций

---
+++ src/store/ducks/recommendation/selectors.ts
@@ -0,0 +1,22 @@
+import { RootState } from '../../store';
+import { RecommendationState } from './contracts/state';
+
+export const selectRecommendationState = (
+  state: RootState
+): RecommendationState => state.recommendation;
+
+export const selectRecommendationData = (
+  state: RootState
+): RecommendationState['recommendation'] =>
+  selectRecommendationState(state).recommendation;
+
+export const selectRecommendationResponse = (
+  state: RootState
+): RecommendationState['response'] => selectRecommendationState(state).response;
+
+export const selectRecommendationStatus = (
+  state: RootState
+): RecommendationState['status'] => selectRecommendationState(state).status;
+
+export const selectRecommendationLoadingStatus = (state: RootState): string =>
+  selectRecommendationState(state).status;
---
+++ src/store/ducks/recommendations/actionCreators.ts
@@ -0,0 +1,40 @@
+import {
+  FetchRecommendationsDataActionInterface,
+  SetRecommendationsDataActionInterface,
+  SetRecommendationsLoadingStatusActionInterface,
+  SetRecommendationsResponseActionInterface,
+  RecommendationsActionsType,
+} from './contracts/actionTypes';
+import { RecommendationsState } from './contracts/state';
+
+export const fetchRecommendationsData =
+  (): FetchRecommendationsDataActionInterface => ({
+    type: RecommendationsActionsType.FETCH_RECOMMENDATIONS_DATA,
+  });
+
+export const setRecommendationsLoadingStatus = (
+  payload: RecommendationsState['status']
+): SetRecommendationsLoadingStatusActionInterface => ({
+  type: RecommendationsActionsType.SET_LOADING_STATE,
+  payload,
+});
+
+export const setRecommendationsData = (
+  payload: RecommendationsState['recommendations']
+): SetRecommendationsDataActionInterface => ({
+  type: RecommendationsActionsType.SET_RECOMMENDATIONS_DATA,
+  payload,
+});
+
+export const setRecommendationsResponse = (
+  payload: RecommendationsState['response']
+): SetRecommendationsResponseActionInterface => ({
+  type: RecommendationsActionsType.SET_RECOMMENDATIONS_RESPONSE,
+  payload,
+});
+
+export type RecommendationsActions =
+  | SetRecommendationsDataActionInterface
+  | SetRecommendationsLoadingStatusActionInterface
+  | SetRecommendationsResponseActionInterface
+  | SetRecommendationsDataActionInterface;
