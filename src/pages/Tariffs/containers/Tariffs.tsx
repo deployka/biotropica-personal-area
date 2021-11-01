4a1fbbc307cff178b430239f622be9a7fb7845bf
Mikhail <mikhail@Mikhails-MacBook-Pro.local>
2021-11-01 19:35:49 +0300

    Добавлен мобильный компенент карточки (пока без анимации)

--- src/pages/Tariffs/containers/Tariffs.tsx
+++ src/pages/Tariffs/containers/Tariffs.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import { Tariff } from '../Tariff/Tariff';
-import s from './Tariffs.module.scss';
+import { Tariff } from "../components/Tariff/Tariff";
+import { TariffMobile } from "../components/TariffMobile/TariffMobile";
+
+import s from "./Tariffs.module.scss";
+
 interface Props {}
+
 export interface Tariff {
   price: string;
   name: string;
@@ -13,51 +17,70 @@ export interface Tariff {
 export const Tariffs = () => {
   const tariffsTest: Tariff[] = [
     {
-      price: '15 999',
-      name: 'Базовый пакет',
+      price: "15 999",
+      name: "Базовый пакет",
       features: [
-        'Интерпретация результатов анализов',
-        'Рекомендации тренера',
-        'Рекомендации диетолога',
-        'Рекомендации нутрициолога',
+        "Интерпретация результатов анализов",
+        "Рекомендации тренера",
+        "Рекомендации диетолога",
+        "Рекомендации нутрициолога",
       ],
-      prolongLink: 'bibi',
+      prolongLink: "bibi",
     },
     {
-      price: '25 999',
-      name: 'Расширенный пакет',
+      price: "25 999",
+      name: "Расширенный пакет",
       features: [
-        'Интерпретация результатов анализов',
-        'Рекомендации тренера',
-        'Рекомендации диетолога',
-        'Рекомендации нутрициолога',
-        'Рекомендации психолога',
-        'Рекомендации эндокринолога',
+        "Интерпретация результатов анализов",
+        "Рекомендации тренера",
+        "Рекомендации диетолога",
+        "Рекомендации нутрициолога",
+        "Рекомендации психолога",
+        "Рекомендации эндокринолога",
       ],
-      prolongLink: 'bibi',
+      prolongLink: "bibi",
     },
     {
-      price: '35 999',
-      name: 'Индивидуальный пакет',
+      price: "35 999",
+      name: "Индивидуальный пакет",
       features: [
-        'Интерпретация результатов анализов',
-        'Консультации тренера по видеосвязи',
-        'Консультации диетолога по видеосвязи',
-        'Консультации нутрициолога по видеосвязи',
-        'Консультации психолога по видеосвязи',
-        'Консультации эндокринолога по видеосвязи',
+        "Интерпретация результатов анализов",
+        "Консультации тренера по видеосвязи",
+        "Консультации диетолога по видеосвязи",
+        "Консультации нутрициолога по видеосвязи",
+        "Консультации психолога по видеосвязи",
+        "Консультации эндокринолога по видеосвязи",
       ],
-      prolongLink: 'bibi',
+      prolongLink: "bibi",
     },
   ];
+
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    if (document.documentElement.clientWidth <= 500) {
+      setMobile(true);
+    }
+  }, []);
+
   return (
     <div className={s.tariffs}>
-      {tariffsTest.map((currentTariff: Tariff, i) => (
-        <Tariff
-          key={`${currentTariff.name}_${currentTariff.price}_${i}`}
-          tariff={currentTariff}
-        />
-      ))}
+      {tariffsTest.map((currentTariff: Tariff, i) => {
+        if (mobile) {
+          return (
+            <TariffMobile
+              key={`${currentTariff.name}_${currentTariff.price}_${i}`}
+              tariff={currentTariff}
+            />
+          );
+        }
+        return (
+          <Tariff
+            key={`${currentTariff.name}_${currentTariff.price}_${i}`}
+            tariff={currentTariff}
+          />
+        );
+      })}
     </div>
   );
 };
