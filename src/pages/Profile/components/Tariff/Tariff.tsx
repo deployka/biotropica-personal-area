744b95bd3c3fed296a47e2cbffb92223b5ff372f
Mikhail <mikhail@Mikhails-MacBook-Pro.local>
2021-11-23 00:16:48 +0300

    промежуточный коммит для мержа с девелоп

--- src/pages/Profile/components/Tariff/Tariff.tsx
+++ src/pages/Profile/components/Tariff/Tariff.tsx
@@ -1,25 +1,23 @@
-import { Link } from "react-router-dom";
-import s from "./Tariff.module.scss";
+import { Link } from 'react-router-dom';
+import s from './Tariff.module.scss';
 interface Props {
   Tariff: any; //TODO: Добавить интерфейс после добавления тарифов в редакс
 }
 
 export const Tariff = ({ Tariff }: Props) => {
   return (
-    <Link style={{ textDecoration: "none" }} to="/tariffs">
-      <div className={s.tariff}>
-        <div className={s.title}>
-          <p>
-            Тариф {"  "}
-            {Tariff.name}
-          </p>
-        </div>
-        <div className={s.date}>
-          <p>
-            до {"  "}
-            {Tariff.expires}
-          </p>
-        </div>
+    <Link style={{ textDecoration: 'none' }} className={s.tariff} to="/tariffs">
+      <div className={s.title}>
+        <p>
+          Тариф {'  '}
+          {Tariff.name}
+        </p>
+      </div>
+      <div className={s.date}>
+        <p>
+          до {'  '}
+          {Tariff.expires}
+        </p>
       </div>
     </Link>
   );
