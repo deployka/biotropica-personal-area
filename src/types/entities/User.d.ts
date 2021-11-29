48e4ff5a60cb1943b2af693e70d0d1472dd9f90a
MirageN1349 <desertirpipi@gmail.com>
2021-11-29 15:09:26 +0300

    пофиксил модель юзера

--- src/types/entities/User.d.ts
+++ src/types/entities/User.d.ts
@@ -1,24 +1,21 @@
 interface User {
-    id: number;
-    email: string;
-    password: string;
-    profile_photo: string;
-    name: string;
-    lastname: string;
-    patronymic: string;
-    dob: Date;
-    phone: string;
-    gender: {
-        value: string;
-        label: string;
-    }[];
-    before_photos: JSON;
-    after_photos: JSON;
-    paid: boolean;
-    isOnline: boolean;
-    banned: boolean;
-    banReason: string;
-    confirmed: boolean;
-    confirmed_hash: string;
-    refreshToken: string;
+  id: number;
+  email: string;
+  password: string;
+  profile_photo: string;
+  name: string;
+  lastname: string;
+  patronymic: string;
+  dob: Date;
+  phone: string;
+  gender: {
+    value: string;
+    label: string;
+  }[];
+  paid: boolean;
+  isOnline: boolean;
+  banned: boolean;
+  banReason: string;
+  confirmed: boolean;
+  refreshToken: string;
 }
