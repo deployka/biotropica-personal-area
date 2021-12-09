215d4270190798224236156f8805876c3daacb94
zolkinka <32388803+zolkinka@users.noreply.github.com>
2021-12-09 23:45:21 +0300

    починил чат

--- src/shared/Global/Chat/services/chatApi.ts
+++ src/shared/Global/Chat/services/chatApi.ts
@@ -1,3 +1,5 @@
+import $api from "../../../../http";
+
 const chatUrl = process.env.REACT_APP_CHAT_URL;
 
 export const chatApi = {
@@ -6,20 +8,11 @@ export const chatApi = {
         this.token = token;
     },
     async fetchDialog(dialogId: number): Promise<Dialog> {
-        const rawDialog = await fetch(chatUrl + '/dialogs/' + dialogId, {
-            credentials: 'include',
-        })
-        return rawDialog.json();
+        const {data} = await $api.get('/dialogs/' + dialogId);
+        return data;
     },
     async fetchDialogs(): Promise<Dialog[]> {
-        const rawDialogs = await fetch(chatUrl + '/dialogs/', {
-            credentials: 'include',
-        })
-        const response = await rawDialogs.json();
-
-        if (response.statusCode) {
-            throw response;
-        }
-        return response
+        const {data} = await $api.get('/dialogs/');
+        return data;
     }
 }
