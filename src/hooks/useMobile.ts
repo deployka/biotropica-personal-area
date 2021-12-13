26ad3c966ca18f1b6672492968651cab612749d0
Mikhail <mikhail@newshift.ru>
2021-12-13 16:04:52 +0300

    фиксы

--- src/hooks/useMobile.ts
+++ src/hooks/useMobile.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 export const useMobile = () => {
   const [isMobile, setIsMobile] = useState(false);
   useEffect(() => {
-    if (document.documentElement.clientWidth <= 820) {
+    if (document.documentElement.clientWidth <= 600) {
       setIsMobile(true);
     } else {
       setIsMobile(false);
