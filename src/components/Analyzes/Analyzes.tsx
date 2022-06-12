f0c2997724b1b9cea208e3860893835ef7b3882c
MikleBr <barulin.ma@gmail.com>
2022-06-12 15:25:09 +0300

    Насмтроил логику страницы анализов

---
+++ src/components/Analyzes/Analyzes.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { AnalyzesCard } from './Card/Card';
+import { BaseUser } from '../../@types/entities/BaseUser';
+import { IInfoBar, InfoBar } from '../../shared/Global/InfoBar/InfoBar';
+import { CreateAnalyzeAnswerDto } from '../../@types/dto/analyzes/create.dto';
+import s from './Analyzes.module.scss';
+import { AnalyzesAddModal } from './AddModal/AddModal';
+import { AnalyzeAnswer } from '../../@types/entities/AnalyzeAnswer';
+import { Analyze } from '../../@types/entities/Analyze';
+
+interface Props {
+  isAnalyzesLoading: boolean;
+  isAddAnalyzeLoading: boolean;
+  isModalOpen: boolean;
+  analyzes: AnalyzeAnswer[];
+  analyzeTypes: Analyze[];
+  setIsModalOpen: (isOpen: boolean) => void;
+  onAddAnalyze: (values: CreateAnalyzeAnswerDto) => void;
+}
+
+export const Analyzes = ({
+  onAddAnalyze,
+  isModalOpen,
+  analyzes,
+  analyzeTypes,
+  isAnalyzesLoading,
+  setIsModalOpen,
+  isAddAnalyzeLoading,
+}: Props) => {
+  const [isShowMore, setIsShowMore] = useState(false);
+
+  function onShowMoreClick() {
+    setIsShowMore(!isShowMore);
+  }
+
+  const analyzesInfoBar: IInfoBar = {
+    title: 'Вы не добавляли анализы',
+    text: 'У вас нет загруженных анализов.',
+    bottomLink: 'Загрузить анализы',
+    onClick: () => {
+      setIsModalOpen(true);
+    },
+  };
+
+  if (isAnalyzesLoading) {
+    return (
+      <div className={s.analyzes}>
+        <p>Загрузка...</p>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className={s.analyzes}>
+        {analyzes.length ? (
+          <AnalyzesCard
+            analyzeTypes={analyzeTypes}
+            onAddAnalyzeClick={() => setIsModalOpen(true)}
+            analyzes={analyzes}
+            onShowMoreClick={onShowMoreClick}
+            isShowMore={isShowMore}
+          />
+        ) : (
+          <InfoBar infoBar={analyzesInfoBar} />
+        )}
+      </div>
+      {/* FIXME: вынести isOpen внутрь модалки */}
+      {isModalOpen && (
+        <AnalyzesAddModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          onSubmit={onAddAnalyze}
+          isLoading={isAddAnalyzeLoading}
+        />
+      )}
+    </>
+  );
+};
