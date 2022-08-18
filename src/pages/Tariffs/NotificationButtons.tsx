5c1af8ba73d5215c817fc4fcdddfceb8dbe4a5a5
MikleBr <barulin.ma@gmail.com>
2022-08-18 17:48:27 +0300

    фикс логики и стилей при продлении/смене тарифа

--- src/pages/Tariffs/NotificationButtons.tsx
+++ src/pages/Tariffs/NotificationButtons.tsx
@@ -2,43 +2,39 @@ import React from 'react';
 import { Button } from '../../shared/Form/Button/Button';
 
 type Props = {
+  type: 'extend' | 'replace';
   onDiscard: () => void;
   onChange: () => void;
 };
 
-export const NotificationButtons = ({ onDiscard, onChange }: Props) => {
+export const NotificationButtons = ({ type, onDiscard, onChange }: Props) => {
   return (
     <>
       <Button
         style={{
           marginRight: '20px',
-          color: '#fff',
           marginBottom: '5px',
           marginTop: '5px',
-          border: '1px solid #fff',
-          background: 'none ',
         }}
         name="discard"
         onClick={onChange}
         options={{
-          content: 'Заменить',
+          content: type === 'extend' ? 'Продлить' : 'Заменить',
           width: '100px',
           height: '30px',
-          classes: { discard: true },
         }}
       />
       <Button
         style={{
           marginBottom: '5px',
           marginTop: '5px',
-          background: '#fff',
-          color: '#000',
         }}
         onClick={onDiscard}
         options={{
           content: 'Отмена',
           width: '100px',
           height: '30px',
+          classes: { discard: true },
         }}
       />
     </>
