324cbda1f5a12cc37e0cb4c7c97693c46825b492
Mikhail Barulin <mikhail-barulin@mail.ru>
2022-09-28 00:31:30 +0300

    feat: 🎸 компонент NumberInput

---
+++ src/shared/Form/NumberInput/NumberInput.tsx
@@ -0,0 +1,73 @@
+import React, {
+  ClipboardEventHandler,
+  FormEventHandler,
+  KeyboardEventHandler,
+  ChangeEventHandler,
+  ChangeEvent,
+} from 'react';
+import classNames from 'classnames';
+
+import s from './NumberInput.module.scss';
+import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
+import { Label } from '../Label/Label';
+import { FormikErrors, FormikTouched } from 'formik';
+
+export type Classes = {
+  [x in string]: boolean;
+};
+
+interface Props {
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  onPaste?: ClipboardEventHandler<HTMLInputElement>;
+  onKeyDown?: KeyboardEventHandler<HTMLInputElement>;
+  onInput?: FormEventHandler<HTMLInputElement>;
+  onBlur?: ChangeEventHandler<HTMLInputElement>;
+  autoComplete?: string;
+  placeholder?: string;
+  label?: string;
+  name: string;
+  value?: number;
+  type?: string;
+  options?: {
+    classes?: Classes;
+    touched: FormikTouched<{ [key in string]: unknown }>;
+    errors: FormikErrors<{ [key in string]: unknown }>;
+    placeholderActive?: string;
+  };
+}
+
+export const NumberInput = ({ onChange, ...props }: Props) => {
+  const { options, ...inputProps } = props;
+  const { touched, errors, classes } = options || { touched: {}, errors: {} };
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    if (isNaN(+e.target.value)) return;
+
+    if (onChange) {
+      onChange(e);
+    }
+  };
+
+  return (
+    <>
+      <input
+        className={classNames({
+          ...classes,
+          [s.input]: true,
+          [s.active]: !!props.value,
+          [s.success__input]: touched[props.name] && !errors[props.name],
+          [s.error__input]: touched[props.name] && errors[props.name],
+        })}
+        {...inputProps}
+        onInput={handleInput}
+        placeholder={props.placeholder}
+      />
+      {props.label && (
+        <Label active={props.value !== undefined} value={props.label} />
+      )}
+      {touched[props.name] && errors[props.name] && (
+        <ErrorMessage message={errors[props.name] || ''} />
+      )}
+    </>
+  );
+};
