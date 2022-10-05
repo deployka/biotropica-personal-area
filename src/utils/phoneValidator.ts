355a6fad0fcb378dcf8e53ced701aaf537e56bc9
Mikhail Barulin <mikhail-barulin@mail.ru>
2022-10-05 15:19:14 +0300

    fix: 🐛 изменил маску и валидацию телефона

--- src/utils/phoneValidator.ts
+++ src/utils/phoneValidator.ts
@@ -18,8 +18,10 @@ export const onPhonePaste = function (e: ClipboardEvent<HTMLInputElement>) {
 
 export const onPhoneInput = function (e: FormEvent<HTMLInputElement>) {
   const input = e.target as HTMLInputElement;
-  let inputNumbersValue = getInputNumbersValue(input);
+  const inputNumbersValue = getInputNumbersValue(input);
+  console.log('inputNumbersValue :', inputNumbersValue);
   const selectionStart = input.selectionStart;
+  console.log('selectionStart :', selectionStart);
   let formattedInputValue = '';
 
   if (!inputNumbersValue) {
@@ -33,25 +35,7 @@ export const onPhoneInput = function (e: FormEvent<HTMLInputElement>) {
     return;
   }
 
-  if (['7', '8', '9'].indexOf(inputNumbersValue[0]) > -1) {
-    if (inputNumbersValue[0] === '9') inputNumbersValue = '7' + inputNumbersValue;
-    const firstSymbols = '+7';
-    formattedInputValue = input.value = firstSymbols + ' ';
-    if (inputNumbersValue.length > 1) {
-      formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
-    }
-    if (inputNumbersValue.length >= 5) {
-      formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
-    }
-    if (inputNumbersValue.length >= 8) {
-      formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
-    }
-    if (inputNumbersValue.length >= 10) {
-      formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
-    }
-  } else {
-    formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
-  }
+  formattedInputValue = '+' + inputNumbersValue.substring(0, 15);
   input.value = formattedInputValue;
 };
 export const onPhoneKeyDown = function (e: KeyboardEvent<HTMLInputElement>) {
