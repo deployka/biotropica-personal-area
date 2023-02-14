7eee2244aefd0d1fab44754142318e4dd4fc2042
Igor Gorokhov <igor.gorokhov@arcadia.spb.ru>
2023-02-14 17:28:00 +0400

    add firebase messaging sw

--- src/sw/firebaseMessagingSw.ts
+++ src/sw/firebaseMessagingSw.ts
@@ -1,15 +1,28 @@
-importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
-importScripts(
-  'https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js',
-);
-firebase.initializeApp({
+import { initializeApp } from 'firebase/app';
+import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw';
+
+const firebaseApp = initializeApp({
   apiKey: process.env.FB_API_KEY,
   authDomain: process.env.FB_AUTH_DOMAIN,
   projectId: process.env.FB_PROJECT_ID,
   storageBucket: process.env.FB_STORAGE_BUCKET,
   messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
   appId: process.env.FB_APP_ID,
-  measurementId: process.env.FB_MEASUREMENT_ID,
 });
-firebase.messaging();
+
+const messaging = getMessaging(firebaseApp);
+
+// onBackgroundMessage(messaging, (payload) => {
+//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//   // Customize notification here
+//   const notificationTitle = 'Background Message Title';
+//   const notificationOptions = {
+//     body: 'Background Message body.',
+//     icon: '/firebase-logo.png'
+//   };
+
+//   self.registration.showNotification(notificationTitle,
+//     notificationOptions);
+// });
+
 export {};
