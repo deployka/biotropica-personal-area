9ea2e5ed80c4646b1e6306cd144c7f81543ebfba
Vladislav Serebrov <48473470+FallenHopes@users.noreply.github.com>
2023-03-23 22:57:37 +0300

    Update Header.tsx

--- src/components/Task/BaseEditor/Header.tsx
+++ src/components/Task/BaseEditor/Header.tsx
@@ -184,6 +184,7 @@ export const Header = ({
                 }}
               />
             </div>
+            }
           </div>
           <div className={s.taskInfo}>
             <div className={s.row}>
