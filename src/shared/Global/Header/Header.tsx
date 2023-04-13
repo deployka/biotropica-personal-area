3910fc7dbb29408e74db83e91feb99d781292ea5
Sargass <alexey.simochkin@gmail.com>
2023-04-13 18:55:26 +0300

    fix header and rename tab opovecheniye

--- src/shared/Global/Header/Header.tsx
+++ src/shared/Global/Header/Header.tsx
@@ -1,6 +1,8 @@
 import React, { Dispatch, memo, ReactElement, SetStateAction } from 'react';
 import { HeaderSvgSelector } from '../../../assets/icons/header/HeaderSvgSelector';
 
+import headerIcon  from '../../../assets/icons/temp/logo.png';
+
 import s from './Header.module.scss';
 import classNames from 'classnames';
 import { useMobile } from '../../../hooks/useMobile';
@@ -27,9 +29,15 @@ export const Header = memo(
     return (
       <header className={s.header}>
         <div className={s.header__wrapper}>
-          <div className={s.header__title}>
-            <h2>{page}</h2>
-          </div>
+           <div className={s.header__headerblock_icon}>
+              <div>
+                <img src={headerIcon} height='30px'/>
+              </div>
+              <div className={s.header__title}>
+                <h2>{page}</h2>
+              </div>
+           </div>
+         
           {centerComponent || ''}
           {!isMobile && (
             <div className={s.header__links}>
