87cc906d736bea4306d0c85e5e55bfae42da08ce
Igor Gorokhov <iorgvi@gmail.com>
2023-06-02 20:57:10 +0400

    fix calendar

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -3,7 +3,7 @@ import { CalendarDay, CalendarDayType } from './Day/Day';
 import { formatISO } from 'date-fns';
 
 import s from './TaskCalendar.module.scss';
-import { getCalendarPageDays } from './TaskCalendarHelper';
+import { convertDateToCalendarDayType, getCalendarPageDays } from './TaskCalendarHelper';
 import { SomeTask } from '../../@types/entities/Task';
 
 interface Props {
@@ -13,38 +13,17 @@ interface Props {
   doneButtonHandler(): void;
 }
 
+
 export const Calendar = ({ tasks, currentMonth, onClickTask, doneButtonHandler }: Props) => {
   const [daysWithTasks, setDaysWithTasks] = useState<CalendarDayType[]>([]);
 
   useEffect(() => {
-    let isPast = true;
+
     const period = getCalendarPageDays(currentMonth);
     const now = new Date();
-    const currentDate = formatISO(now).slice(0, 10);
 
     setDaysWithTasks(
-      period.map(date => {
-        // const month = date.slice(0, 7);
-        const day = date.slice(8, 10);
-        if (currentDate === date) {
-          isPast = false;
-        }
-
-        const nameOfDay = new Date(
-          now.getFullYear(),
-          now.getMonth(),
-          +day,
-        ).toLocaleString('ru', { weekday: 'short' });
-
-        return {
-          isPast,
-          isGrey: false,
-          isCurrentDay: currentDate === date,
-          day: +day,
-          nameOfDay,
-          tasks: tasks.filter(task => task.date === date),
-        };
-      }),
+      period.map(date => convertDateToCalendarDayType(date, now, tasks)),
     );
   }, [tasks, currentMonth]);
 
